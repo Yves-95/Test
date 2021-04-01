e46aef0125c66f799c2137f9dab0d7e7da21774d
Yves-95 <77168739+Yves-95@users.noreply.github.com>
2021-04-01 20:06:46 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+
+
+SQLALCHEMY_DATABASE_URI = "postgres+psycopg2://postgres:docker@localhost:54327/corporeal_test"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    url = db.Column(db.String)
+    status = db.Column(db.String)
+
+    def __init__(self, name):
+        self.name = name
+
+    # Gets dict with the Doctor object and all of its associated reviews
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+            'status': [self.status]
+        }
